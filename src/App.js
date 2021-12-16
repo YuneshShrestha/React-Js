ce17b6841e479bf19e486e0f8ad28d8114cbbae8
Yunesh Shrestha <yuneshshrestha24@gmail.com>
2021-12-16 19:12:45 +0545

    useeffect json

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import React from "react";
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import UseEffectProgram from "./UseEffectProgram";
+// import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 const App=()=>{
 
     return (
-        <div className="container">
-            <h1 className="text-center">Welcome</h1>
-            <div class="card">
-            <h5 class="card-header">Featured</h5>
-            <div class="card-body">
-                <h5 class="card-title">Special title treatment</h5>
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-            </div>
-            <button className="btn btn-outline-danger ">Hello</button>
-        </div>
+       <UseEffectProgram/>
     );
 };
 export default App;
---
+++ src/UseEffectProgram.js
@@ -0,0 +1,32 @@
+import React,{useState,useEffect} from 'react';
+
+function UseEffectProgram() {
+    let [resourceType, setResourceType]=useState("Posts");
+    let [items, setItems] = useState([]);
+    useEffect(()=>{
+        fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+        .then(response => response.json())
+        .then(json => setItems(json))
+    },[resourceType]);
+    return (
+        <div>
+           <div>
+            <button onClick={()=>setResourceType("Posts")}>Post</button>
+            <button onClick={()=>setResourceType("Users")}>Users</button>
+            <button onClick={()=>setResourceType("Comments")}>Comments</button>
+            {/* <button>Hello</button> */}
+
+           </div>
+           <h3>{resourceType}</h3>
+
+            {
+                items.map(item=>{
+                    return <pre>{JSON.stringify(item)}</pre>
+                })
+            }
+
+        </div>
+    );
+}
+
+export default UseEffectProgram;
